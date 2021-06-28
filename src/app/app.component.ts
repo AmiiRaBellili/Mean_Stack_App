17646b58300eca254d6cae6a28cf1cf7ae1a63f1
AmiiRaBellili <belliliamiira@gmail.com>
2021-06-28 16:18:06 +0100

    Creating posts with property and events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mean-stack';
+  storedPosts=[]; 
+
+  onPostAdded(post){
+
+    this.storedPosts.push(post);
+  }
 }
--- src/app/posts/post-create.componenet.ts
+++ src/app/posts/post-create.componenet.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
     selector:'app-post-create',
-    templateUrl:'./post-create.component.html'
+    templateUrl:'./post-create.component.html',
+    styleUrls :['./post-create.component.css']
 })
 export class PostCreateComponent{
-    entredValue = '';
-    AddPost = "No content";
-  onAddPost(){
-     this.AddPost=this.entredValue;
-  }
+    entredTitle = '';
+    entredContent = '';
+   @Output() postCreated = new EventEmitter(); 
+    onAddPost(){
+        const post = {
+          title: this.entredTitle,
+
+          content:this.entredContent
+                 };
+        this.postCreated.emit(post);
+    }
+  
 }
 
